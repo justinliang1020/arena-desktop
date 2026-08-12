13a0046e4f552c848c4a381c8252027957cf08ea
Justin Liang <justinliang1020@gmail.com>
2026-08-12 00:25:26 -0400

    return false for are.na subdomains

--- src/utils.cjs
+++ src/utils.cjs
@@ -1,12 +1,14 @@
 const { BrowserWindow, nativeTheme } = require("electron");
 
 /**
+ * only returns true for are.na
+ * returns false for non-are.na website or subodmains such as help.are.na
  * @param {string} url
  */
 function isArenaUrl(url) {
   try {
     const { hostname } = new URL(url);
-    return hostname === "are.na" || hostname.endsWith(".are.na");
+    return hostname === "are.na" || hostname === "www.are.na";
   } catch {
     return false;
   }
